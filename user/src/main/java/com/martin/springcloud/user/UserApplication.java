343bba877261695c970c7c12f3c792c5c8fe31fd
MartinZengg <28219714@qq.com>
2019-04-23 10:25:29 +0800

    增加了hystrix 失败策略

--- user/src/main/java/com/martin/springcloud/user/UserApplication.java
+++ user/src/main/java/com/martin/springcloud/user/UserApplication.java
@@ -15,9 +15,11 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 @EnableEurekaClient
+//简化服务间调用
 @EnableFeignClients
 //用代码的方式改变均衡负载策略
 //@RibbonClient(name = "zengguoqiangorder", configuration = RibbonChooseConfiguration.class)
+//hystrix 失败策略
 @EnableCircuitBreaker
 public class UserApplication {
 
