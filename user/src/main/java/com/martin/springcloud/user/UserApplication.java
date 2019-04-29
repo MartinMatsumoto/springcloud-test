070c208f2a69c45a20acdb532dc89b3eafc747ca
MartinZengg <28219714@qq.com>
2019-04-29 10:43:43 +0800

    自定义Zuul回退机制处理器

--- user/src/main/java/com/martin/springcloud/user/UserApplication.java
+++ user/src/main/java/com/martin/springcloud/user/UserApplication.java
@@ -11,7 +11,6 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
